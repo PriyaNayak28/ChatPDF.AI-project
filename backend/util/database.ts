031a3c153dfe9cbc9645f03f0152990dfdbe8b14
PriyaNayak20 <priya.n@webkorps.com>
2025-05-14 21:11:59 +0530

    files added

--- backend/util/database.ts
+++ backend/util/database.ts
@@ -1,14 +1,29 @@
+// import { Sequelize } from 'sequelize'
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME!,
+//   process.env.DB_USER!,
+//   process.env.DB_PASS!,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     logging: false,
+//   }
+// )
+
+// export default sequelize
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASS!,
+  process.env.DATABASE_URL ||
+    `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   {
-    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false,
   }
